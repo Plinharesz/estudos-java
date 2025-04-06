16911ef2fd3670d0d384f377af7b1f7ab8ecf2c4
plinhares <plinfps@gmail.com>
2025-04-06 14:21:51 -0300

    Criação do conversor de temperatura Celsius para Fahrenheit

---
+++ alura/introducao/ConvertorTemperatura.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ConvertorTemperatura {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar a temperatura em graus pro usuário
+        System.out.print("Digite a temperatura em graus Celcius: ");
+        int grausCelcius = scanner.nextInt();
+
+        //Conversor
+        int grausFahrenheit = (int)((grausCelcius * 1.8) + 32);
+                System.out.printf("%d graus Celsius é equivalente à %d graus Fahrenheit.", grausCelcius, grausFahrenheit);
+    }
+}
