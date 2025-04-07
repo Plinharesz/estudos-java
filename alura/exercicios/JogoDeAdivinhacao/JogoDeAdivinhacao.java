3fa67bf40653533ca9c8abd7988b57a47374b160
plinhares <plinfps@gmail.com>
2025-04-07 03:54:52 -0300

    Code .java finalizado
    Adicionado o README.md para a pasta do exercicio JogoDeAdivinhacao

--- alura/exercicios/JogoDeAdivinhacao/JogoDeAdivinhacao.java
+++ alura/exercicios/JogoDeAdivinhacao/JogoDeAdivinhacao.java
@@ -1,5 +1,3 @@
-package JogoDeAdivinhacao;
-
 /*
 Agora é com você! Pratique os conceitos que foram ensinados ao longo dessa aula com o seguinte desafio:
 Crie um programa que simula um jogo de adivinhação, que deve gerar um número aleatório entre 0 e 100 e pedir para que o usuário tente adivinhar o número, em até 5 tentativas. A cada tentativa, o programa deve informar se o número digitado pelo usuário é maior ou menor do que o número gerado.
@@ -13,9 +11,44 @@ Utilize um loop para controlar as tentativas;
 Utilize a instrução break; para interromper o loop;
 
  */
+package JogoDeAdivinhacao;
+import java.util.Random;
+import java.util.Scanner;
 public class JogoDeAdivinhacao {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        //
+        System.out.println("""
+                    JOGO DE ADIVINHAÇÃO - Acerte o número entre 1 e 100.
+                -> Você tem 5 tentativas.""");
+
+        int secretNumber = new Random().nextInt(100) + 1; // esse método gera um número de 0 a 99. Por isso o (+1) pra estar entre 1 e 100
+        int palpite = 0;
+        int tentativas = 0;
+
+        while (tentativas < 5) {
+            System.out.print("Faça seu palpite: ");
+            palpite = scanner.nextInt();
+            tentativas++;
+
+            if (palpite == secretNumber) {
+                System.out.println("Parabêns! Você acertou com "+ tentativas +" tentativa(s)");
+                System.out.println("JOGO FINALIZADO");
+                break;
+            } else if (palpite > secretNumber ) {
+                if (tentativas <= 4 ) { // Estrutura pra que no ultimo palpite, nao de o feedback se o numero é menor
+                    System.out.println("ERROU! O número correto é MENOR");
+                    }
+            } else {
+                if (tentativas <= 4) { // Estrutura pra que no ultimo palpite, nao de o feedback se o numero é maior
+                    System.out.println("ERROU! O número correto é MAIOR");
+                }
+            }
+                if (tentativas == 5) {
+                    System.out.println("GAME OVER");
+            }
+        }
 
     }
 }
---
+++ alura/exercicios/JogoDeAdivinhacao/README.md
@@ -0,0 +1,3 @@
+# Jogo de Adivinhação 
+
+Este exercício consiste em criar um programa interativo entre o usuário e o computador. O computador gera um número entre 1 e 100 e o usuario tenta acertar em no máximo 5 tentativas. Durante os palpites, o sistema da um feedback se o número secreto é maior ou menor que o número sorteado.
